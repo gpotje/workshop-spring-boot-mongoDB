50ddcacea4517cc2d9834ac060efe6e35951b3c8
GPot <gpotje.souza@gmail.com>
2020-09-26 19:45:09 -0300

    projeto criado

---
+++ src/main/java/com/gpotgames/curso/WorkshoppingmongoApplication.java
@@ -0,0 +1,13 @@
+package com.gpotgames.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoppingmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoppingmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpotgames/curso/WorkshoppingmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpotgames.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoppingmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
