5425ed81d7fe589566d852ed2e9c64cab4c6fbaa
GPot <gpotje.souza@gmail.com>
2020-09-29 21:08:53 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/gpotgames/curso/resources/UserResource.java
+++ src/main/java/com/gpotgames/curso/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.gpotgames.curso.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gpotgames.curso.domain.User;
+import com.gpotgames.curso.dto.UserDTO;
 import com.gpotgames.curso.service.UserService;
 
 @RestController
@@ -21,10 +24,10 @@ public class UserResource {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
