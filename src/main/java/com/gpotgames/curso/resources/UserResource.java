d1df75532ecec44cdce6c7c22c199a5970a38c81
GPot <gpotje.souza@gmail.com>
2020-10-01 20:45:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/gpotgames/curso/resources/UserResource.java
+++ src/main/java/com/gpotgames/curso/resources/UserResource.java
@@ -1,16 +1,19 @@
 package com.gpotgames.curso.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gpotgames.curso.domain.User;
 import com.gpotgames.curso.dto.UserDTO;
@@ -37,5 +40,16 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+				}
+
 
 }
