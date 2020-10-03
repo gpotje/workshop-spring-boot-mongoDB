1229624c5634bbcc4bf2251049162a7c4b4ab48c
GPot <gpotje.souza@gmail.com>
2020-10-02 21:46:45 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gpotgames/curso/service/PostService.java
@@ -0,0 +1,28 @@
+package com.gpotgames.curso.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gpotgames.curso.domain.Post;
+import com.gpotgames.curso.repository.PostRepository;
+import com.gpotgames.curso.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+
+	
+
+}
