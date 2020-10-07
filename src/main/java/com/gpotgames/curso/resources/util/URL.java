dc275d2ee6d0d5bdc6dc6574dfd727e7b7e87475
GPot <gpotje.souza@gmail.com>
2020-10-06 21:15:19 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/gpotgames/curso/resources/util/URL.java
@@ -0,0 +1,18 @@
+package com.gpotgames.curso.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParm(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			
+			return "";
+		}
+		
+	}
+	
+}
